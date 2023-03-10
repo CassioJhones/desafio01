1d771a74270cdb1754557833efe6e04af1cdb552
Cassio Jhones <cassio.bjhones@gmail.com>
2023-03-10 00:21:51 -0300

    desafio

---
+++ main.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class main {
+  public static void main(String[] args) throws IOException {
+     Scanner leitor = new Scanner(System.in);
+
+     int positivos = 0;
+     double media = 0;
+     double somaPositivos = 0;
+     double x = 0 ;
+        
+        for (int contador = 0 ; contador < 6; contador++) {
+       // 	System.out.print("--DIGITE O " + (contador + 1) + "º NUMERO:  ");
+            x = leitor.nextDouble();
+
+           if (x > 0){
+             positivos ++;
+             somaPositivos += x;
+            }
+        	}
+            media = somaPositivos / positivos;
+
+            System.out.println("\n" + positivos+" valores positivos\n");
+            System.out.println(String.format("%.1f", media));
+            
+        }
+    }
+    
